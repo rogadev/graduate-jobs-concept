d056994bedd3ac5010e85d21f568b76e7e14d928
vonroga <ryanroga@gmail.com>
2022-06-07 12:03:10 -0700

    Add Features
    - Styling improvements
    - Added conditional rendering
    - Changed credential input to select box

--- components/OutputArea.vue
+++ components/OutputArea.vue
@@ -1,37 +1,56 @@
 <script setup>
-defineProps({
+const props = defineProps({
   results: {
     type: Object,
     default: {},
   },
 });
 
-// Title case string, ignoring anything inside of brackets
-function titleCase(str) {
-  const words = str.split(" ");
-  const result = [];
+const hasResults = computed(() => {
+  return Object.keys(props.results).length > 0;
+});
 
-  words.forEach((word) => {
-    result.push(word.charAt(0).toUpperCase() + word.slice(1));
-  });
-  return result.join(" ");
-}
+const hasGraduateJobs = computed(() => {
+  return new Array(props.results.after_graduation)[0].length > 0;
+});
+
+const hasExperiencedJobs = computed(() => {
+  return new Array(props.results.with_experience)[0].length > 0;
+});
 </script>
 
 <template>
   <div>
-    <div v-if="results.length !== {}">
-      <div v-if="results.after_graduation">
+    <div v-if="hasResults">
+      <div v-if="hasGraduateJobs">
         <h2>Applicable Jobs</h2>
-        <div v-for="result of results.after_graduation">
-          {{ titleCase(result) }}
+        <p>
+          This is an exhaustive list of positions you could potentially apply
+          for upon graduation from your program.
+        </p>
+        <div class="results-area">
+          <ResultChip
+            v-for="result of results.after_graduation"
+            :result="result"
+          />
         </div>
       </div>
-      <div v-if="results.with_experience">
+      <div v-if="hasExperiencedJobs">
         <h2>Future Positions</h2>
-        <div v-for="result of results.with_experience">
-          {{ titleCase(result) }}
+        <p>
+          Future positions require a bit more experience in the industry but,
+          with a bit of experience, these are all potential career paths for
+          you.
+        </p>
+        <div class="results-area">
+          <ResultChip
+            v-for="result of results.with_experience"
+            :result="result"
+          />
         </div>
+      </div>
+      <div v-if="!hasGraduateJobs && !hasExperiencedJobs">
+        <p>Sorry, no jobs were returned from your search.</p>
       </div>
     </div>
     <div v-else>
@@ -39,3 +58,23 @@ function titleCase(str) {
     </div>
   </div>
 </template>
+
+<style scoped>
+.results-area {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  padding: 0.5rem;
+}
+@media (max-width: 768px) {
+  .results-area {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+@media (max-width: 480px) {
+  .results-area {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
---
+++ components/ResultChip.vue
@@ -0,0 +1,47 @@
+<script setup>
+const props = defineProps({
+  result: {
+    type: String,
+    required: true,
+  },
+});
+
+/**
+ * Title case string, ignoring anything inside of brackets
+ * @param {string} str  - string to be title cased
+ * @returns {string}    - title cased string
+ */
+function titleCase(str) {
+  const words = str.split(" ");
+  const result = [];
+
+  words.forEach((word) => {
+    result.push(word.charAt(0).toUpperCase() + word.slice(1));
+  });
+  return result.join(" ");
+}
+
+const output = ref(titleCase(props.result));
+</script>
+
+<template>
+  <div class="result">
+    {{ output }}
+  </div>
+</template>
+
+<style scoped>
+.result {
+  background-color: aliceblue;
+  border: 1px solid rgb(200, 227, 250);
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  font-weight: bold;
+  transition: box-shadow 0.1s ease-in-out;
+  cursor: pointer;
+}
+.result:hover {
+  background-color: rgb(221, 239, 255);
+  box-shadow: 3px 3px 5px rgb(204, 204, 204);
+}
+</style>
