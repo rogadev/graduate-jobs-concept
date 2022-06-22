4a0e8857e25a31b39d892efe4a8846d12d0a6b1d
vonroga <ryanroga@gmail.com>
2022-06-22 16:14:40 -0700

    Update /jobs-by-program

--- server/api/v1/jobs-by-program/index.js
+++ server/api/v1/jobs-by-program/index.js
@@ -56,52 +56,99 @@ const checkQuery = (event, query) => {
   }
 }
 
+const searchWithKeywords = async (credential, keywords, duration = false) => {
+  console.log(
+    'Searching jobs-by-credential for: ',
+    credential,
+    typeof keywords,
+    duration
+  )
+  const { jobs, groups } = await $fetch(
+    `/api/v1/jobs-by-credential?credential=${credential}&keywords=${keywords.join(
+      ','
+    )}`
+  )
+  return { jobs, groups }
+}
+
 /**
  * Takes in program NID as query parameter. Returns related jobs based on either NOC program search or credential search via api/v1/jobs-by-credential search.
  */
 export default defineEventHandler(async (event) => {
+  // Used to check query response times.
+  const startTime = Date.now()
   // Query parameter
   const { nid } = useQuery(event) // typeof = string
   // Query parameter error handling
   checkQuery(event, nid)
 
+  // ----------------------------------------------------------------
   let foundResults = false
   let jobResults = []
   let groupResults = []
   let jobCount = 0
   let groupCount = 0
-  // Search for program NID in programs middle table
-  for (const program of programs) {
-    if (program.nid === Number(nid)) {
-      foundResults = true
+  // ----------------------------------------------------------------
 
-      const { jobs, groups } = await $fetch(
-        `/api/v1/jobs-by-credential?credential=${
-          program.credential
-        }&keywords=${program.noc_search_keywords.join(',')}`
-      )
-      for (const job of jobs) {
-        jobResults.push(job)
-        jobCount++
-      }
-      for (const group of groups) {
-        groupResults.push(group)
-        groupCount++
-      }
+  // Find the program that matches the searched NID
+  const program =
+    programs.find((program) => program.nid === Number(nid)) ?? false
+
+  // If no program found, return 404 not found
+  if (!program) {
+    sendError(
+      event,
+      createError({
+        statusCode: 404,
+        statusMessage: `No program found with NID: ${nid}. This may be because this NID is invalid, or it has not yet been connected to valid job searches. Contact web department for details.`,
+        debug: false,
+      })
+    )
+
+    console.log('Returned in:', Date.now() - startTime, 'ms')
+    return
+  } else {
+    // Search for jobs and unit groups based on program search parameters
+    const { jobs, groups } = await searchWithKeywords(
+      program.credential,
+      program.noc_search_keywords,
+      program.duration
+    )
+    // add all jobs to jobResults
+    for (const job of jobs) {
+      foundResults = true
+      jobResults.push(job)
+      jobCount++
+    }
+    // add all groups to groupResults
+    for (const group of groups) {
+      foundResults = true
+      groupResults.push(group)
+      groupCount++
     }
   }
+
   if (foundResults) {
     console.log(
-      `Found ${jobCount} jobs in ${groupCount} unit groups for program NID: ${nid}`
+      `Returned ${jobCount} jobs in ${groupCount} unit groups for program NID: ${nid} in:`,
+      Date.now() - startTime,
+      'ms'
     )
+
     return {
       jobs: Array.from(new Set(jobResults)),
       groups: Array.from(new Set(groupResults)),
     }
   }
 
-  // Respond
-  return {
-    searched: nid,
-  }
+  // Failsafe - something went horribly wrong...
+  console.log('Returned in:', Date.now() - startTime, 'ms')
+
+  return sendError(
+    createError({
+      statusCode: 500,
+      statusMessage: `Something went wrong searching for nid ${nid}.`,
+      debug: false,
+    })
+  )
 })
