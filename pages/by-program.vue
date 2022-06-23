e18dd0193ffd58d3c9e2b1ba1a6394f035513b6f
vonroga <ryanroga@gmail.com>
2022-06-23 15:15:44 -0700

    Create by-program.vue
    New search by program UI

---
+++ pages/by-program.vue
@@ -0,0 +1,37 @@
+<script setup>
+useHead({
+  title: "VIU Graduate Career Outlooks - Careers by Program",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Search for careers and career outlooks in Canada based on a given VIU credential.",
+    },
+  ],
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css",
+      integrity:
+        "sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor",
+      crossorigin: "anonymous",
+    },
+  ],
+  script: [
+    {
+      src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js",
+      integrity:
+        "sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2",
+      crossorigin: "anonymous",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div>
+    <nuxt-link to="/">Back</nuxt-link>
+  </div>
+</template>
